00d3368134291f029db7652cfd34fc8da941eb00
linchuangqiong <linchuangqiong66@163.com>
2021-03-11 19:38:58 +0800

    新增客户端2

---
+++ whale-rpc-example/example-client1/src/main/java/io/github/linxiaobaixcg/ExampleClient1Application.java
@@ -0,0 +1,13 @@
+package io.github.linxiaobaixcg;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleClient1Application.class, args);
+	}
+}
---
+++ whale-rpc-example/example-client1/src/test/java/io/github/linxiaobaixcg/ExampleClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.linxiaobaixcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleClient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
